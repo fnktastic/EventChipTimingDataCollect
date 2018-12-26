cd123502828642dc4440eb9c68d0a6821b2bccc8
fnktastic <VillaNaranjos@yandex.ua>
2018-12-26 18:32:54 +0200

    Set Clock base backend functionality

---
+++ ReaderDataCollector/Reading/TcpDateTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ReaderDataCollector.Reading
+{
+    public class TcpDateTime
+    {
+        const int PORT = 5001;
+        static TcpClient client = new TcpClient();
+
+        public static bool Send(string host, string action)
+        {
+            try
+            {
+                if (host.Equals("localhost"))
+                    host = "127.0.0.1";
+
+                client = new TcpClient(host, PORT);
+                NetworkStream nwStream = client.GetStream();
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(action);
+
+                Console.WriteLine("Sending : " + action);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesReadLength = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+
+                Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesReadLength));
+                client.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}\n{1}", ex.Message, ex.StackTrace);
+                return false;
+            }
+        }
+    }
+}
--- ReaderDataCollector/ViewModel/SetClockViewModel.cs
+++ ReaderDataCollector/ViewModel/SetClockViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Ioc;
 using ReaderDataCollector.Model;
+using ReaderDataCollector.Reading;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +13,41 @@ namespace ReaderDataCollector.ViewModel
 {
     public class SetClockViewModel : ViewModelBase
     {
+        private readonly Reader _reader;
+
         public SetClockViewModel(Reader reader)
         {
-
+            _reader = reader;
         }
 
         [PreferredConstructor]
         public SetClockViewModel() { }
+
+
+        private RelayCommand _setManualTimeCommand;
+        public RelayCommand SetManualTimeCommand
+        {
+            get
+            {
+                return _setManualTimeCommand ?? (_setManualTimeCommand = new RelayCommand(() =>
+                {
+                    string action = string.Format("t1@{0}", 1);
+                    TcpDateTime.Send(_reader.Host, action);
+                }));
+            }
+        }
+
+        private RelayCommand _setSystemTimeCommand;
+        public RelayCommand SetSystemTimeCommand
+        {
+            get
+            {
+                return _setSystemTimeCommand ?? (_setSystemTimeCommand = new RelayCommand(() =>
+                {
+                    string action = string.Format("t2@{0}", DateTime.Now.ToString("HH:mm:ss"));
+                    TcpDateTime.Send(_reader.Host, action);
+                }));
+            }
+        }
     }
 }
