618e4c2cd98b5e0f6f6a813c9d42ff13cd67993a
Alex Fnktastic <fnktastic@outlook.com>
2019-01-04 03:23:37 +0200

    Save reading improvement

--- ReaderDataCollector/Repository/ReadingRepository.cs
+++ ReaderDataCollector/Repository/ReadingRepository.cs
@@ -8,19 +8,33 @@ using System.Threading.Tasks;
 
 namespace ReaderDataCollector.Repository
 {
-    public class ReadingRepository :IReadingRepository
+    public class ReadingRepository : IReadingRepository
     {
         private readonly Context _context;
+        private readonly IReaderRepository _readerRepository;
+        private readonly IReadRepository _readRepository;
 
         public ReadingRepository(Context context)
         {
             _context = context;
+            _readerRepository = new ReaderRepository(_context);
+            _readRepository = new ReadRepository(_context);
         }
 
         public IEnumerable<Reading> Readings => _context.Readings;
 
         public async void SaveReading(Reading reading)
         {
+            var reader = _readerRepository
+                .Readers
+                .FirstOrDefault(r => r.Host == reading.Reader.Host && r.Port == reading.Reader.Port);
+
+            if (reader != null)
+            {
+                reading.Reader = null;
+                reading.ReaderID = reader.ID;
+            }
+
             _context.Readings.Add(reading);
             await _context.SaveChangesAsync();
         }
